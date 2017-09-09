9bdd7115159a9370256caee2133ed6448e16194d
Alexandre Vessoni <av.vessoni@gmail.com>
2017-09-09 14:21:26 -0300

    Novo Produto

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Forms\ProductForm;
 use App\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -26,7 +27,12 @@ class ProductsController extends Controller
      */
     public function create()
     {
-        //
+        $form = \FormBuilder::create(ProductForm::class, [
+            'method' => 'POST',
+            'url' => route('admin.products.store')
+        ]);
+        $title = "Novo Produto";
+        return (view('admin.products.save', compact('form','title')));
     }
 
     /**
---
+++ resources/views/admin/products/save.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <h3 class="panel-title">{{$title}}</h3>
+
+                </div>
+                <div class="panel-body">
+                    <?php $form->add('submit', 'submit', ['label' => '<span class="glyphicon-floppy-disk" aria-hidden="true"></span>'])?>
+                    {!! form($form) !!}
+
+                </div>
+            </div>
+        </div>
+    </div>
+ @endsection
