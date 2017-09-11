3996ce0c15f3be28362f4740e78fda91d8c0a911
Alexandre Vessoni <av.vessoni@gmail.com>
2017-09-11 17:40:12 -0300

    API - Products

---
+++ app/Http/Controllers/Api/ProductsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Product;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProductsController extends Controller
+{
+    public function index(){
+        return Product::all();
+    }
+
+
+
+}
